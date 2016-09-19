63d0da4b459d86d23c9da37455f47c567e80fe94
Tom <Tom@TOMS-DESKTOP>
2016-09-19 08:41:06 -0400

    Removed the white background from the Confirm Restore Point Deletion dialog. It looked ugly as sin.

--- Windows/Confirm Restore Point Deletion.Designer.vb
+++ Windows/Confirm Restore Point Deletion.Designer.vb
@@ -24,12 +24,10 @@ Partial Class frmConfirmDelete
     Private Sub InitializeComponent()
         Me.btnYes = New System.Windows.Forms.Button()
         Me.btnNo = New System.Windows.Forms.Button()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.lblCreatedOn = New System.Windows.Forms.Label()
-        Me.lblRestorePointName = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Panel1.SuspendLayout()
+        Me.lblRestorePointName = New System.Windows.Forms.Label()
+        Me.lblCreatedOn = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -53,64 +51,53 @@ Partial Class frmConfirmDelete
         Me.btnNo.Text = "&No"
         Me.btnNo.UseVisualStyleBackColor = True
         '
-        'Panel1
-        '
-        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Panel1.BackColor = System.Drawing.SystemColors.Window
-        Me.Panel1.Controls.Add(Me.PictureBox1)
-        Me.Panel1.Controls.Add(Me.lblCreatedOn)
-        Me.Panel1.Controls.Add(Me.lblRestorePointName)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(456, 93)
-        Me.Panel1.TabIndex = 5
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(11, 12)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(32, 35)
-        Me.PictureBox1.TabIndex = 8
-        Me.PictureBox1.TabStop = False
-        '
-        'lblCreatedOn
-        '
-        Me.lblCreatedOn.AutoSize = True
-        Me.lblCreatedOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblCreatedOn.Location = New System.Drawing.Point(49, 72)
-        Me.lblCreatedOn.Name = "lblCreatedOn"
-        Me.lblCreatedOn.Size = New System.Drawing.Size(64, 13)
-        Me.lblCreatedOn.TabIndex = 7
-        Me.lblCreatedOn.Text = "Created On:"
-        '
-        'lblRestorePointName
-        '
-        Me.lblRestorePointName.AutoSize = True
-        Me.lblRestorePointName.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblRestorePointName.Location = New System.Drawing.Point(49, 50)
-        Me.lblRestorePointName.Name = "lblRestorePointName"
-        Me.lblRestorePointName.Size = New System.Drawing.Size(105, 13)
-        Me.lblRestorePointName.TabIndex = 6
-        Me.lblRestorePointName.Text = "Restore Point Name:"
-        '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(49, 12)
+        Me.Label1.Location = New System.Drawing.Point(50, 12)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(242, 13)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Are you sure you want to delete this restore point?"
         '
+        'lblRestorePointName
+        '
+        Me.lblRestorePointName.AutoSize = True
+        Me.lblRestorePointName.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRestorePointName.Location = New System.Drawing.Point(50, 50)
+        Me.lblRestorePointName.Name = "lblRestorePointName"
+        Me.lblRestorePointName.Size = New System.Drawing.Size(105, 13)
+        Me.lblRestorePointName.TabIndex = 6
+        Me.lblRestorePointName.Text = "Restore Point Name:"
+        '
+        'lblCreatedOn
+        '
+        Me.lblCreatedOn.AutoSize = True
+        Me.lblCreatedOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCreatedOn.Location = New System.Drawing.Point(50, 72)
+        Me.lblCreatedOn.Name = "lblCreatedOn"
+        Me.lblCreatedOn.Size = New System.Drawing.Size(64, 13)
+        Me.lblCreatedOn.TabIndex = 7
+        Me.lblCreatedOn.Text = "Created On:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 35)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
         'frmConfirmDelete
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(456, 127)
-        Me.Controls.Add(Me.Panel1)
+        Me.ClientSize = New System.Drawing.Size(456, 131)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblCreatedOn)
+        Me.Controls.Add(Me.lblRestorePointName)
         Me.Controls.Add(Me.btnNo)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnYes)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = Global.Restore_Point_Creator.My.Resources.Resources.RestorePoint_noBackground_2
@@ -119,17 +106,15 @@ Partial Class frmConfirmDelete
         Me.MinimumSize = New System.Drawing.Size(472, 166)
         Me.Name = "frmConfirmDelete"
         Me.Text = "Confirm Restore Point Deletion"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents btnYes As Button
     Friend WithEvents btnNo As Button
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents lblCreatedOn As Label
-    Friend WithEvents lblRestorePointName As Label
     Friend WithEvents Label1 As Label
+    Friend WithEvents lblRestorePointName As Label
+    Friend WithEvents lblCreatedOn As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
