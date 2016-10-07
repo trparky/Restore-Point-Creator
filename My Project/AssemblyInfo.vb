c826a2038d779626ded6175b611f53984e40b72e
Tom <Tom@TOMS-DESKTOP>
2016-10-07 10:54:41 -0400

    Bumped version to 5.0 Build 7 Release Candidate 1.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("5.0.6.0")>
+<Assembly: AssemblyFileVersion("5.0.7.0")>
