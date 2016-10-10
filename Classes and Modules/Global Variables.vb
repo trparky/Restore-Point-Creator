a463e1e5d9206b3455a4f7c6b2b5d37f4491e550
Tom <Tom@TOMS-DESKTOP>
2016-10-09 23:03:20 -0400

    Bumped version to 5.0 Build 9 Release Candidate 4.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -68,7 +68,7 @@ Namespace globalVariables
             Public Const shortBetaVersion As Short = 0
 
             Public Const boolReleaseCandidate As Boolean = True
-            Public Const shortReleaseCandidateVersion As Short = 3
+            Public Const shortReleaseCandidateVersion As Short = 4
 #Else
             Public Const boolDebugBuild As Boolean = False
 
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("5.0.8.0")>
+<Assembly: AssemblyFileVersion("5.0.9.0")>
