19481fd078db09489f402944edaa855ba5746426
Tom <Tom@TOMS-DESKTOP>
2016-10-09 22:52:39 -0400

    Added some additional error handling to the customHTTPHelperErrorHandler() that's passed to the HTTPHelper Class. Also removed the error messages boxes that the customHTTPHelperErrorHandler() function presented to the user.

--- Classes and Modules/Functions Namespace/http.vb
+++ Classes and Modules/Functions Namespace/http.vb
@@ -40,10 +40,10 @@
 
         Private Sub customHTTPHelperErrorHandler(ex As Exception, classInstance As httpHelper)
             eventLogFunctions.writeCrashToEventLog(ex)
+            Dim lastAccessedURL As String = classInstance.getLastAccessedURL()
 
             If TypeOf ex Is Net.WebException Then
                 Dim ex2 As Net.WebException = DirectCast(ex, Net.WebException)
-                Dim lastAccessedURL As String = classInstance.getLastAccessedURL()
 
                 If ex2.Status = Net.WebExceptionStatus.ProtocolError Then
                     If ex2.Message.Contains("(500)") = True Then
@@ -56,10 +56,10 @@
                 Else
                     eventLogFunctions.writeToSystemEventLog(String.Format("The server responded with an HTTP Protocol Error while accessing the URL {0}{1}{0}.", Chr(34), lastAccessedURL), EventLogEntryType.Error)
                 End If
-
-                MsgBox("The server responded with an HTTP error, please check the Event Log for more details.", MsgBoxStyle.Critical, "Restore Point Creator")
-            Else
-                MsgBox("A general HTTP error occured, please check the Event Log for more details.", MsgBoxStyle.Critical, "Restore Point Creator")
+            ElseIf TypeOf ex Is httpProtocolException Then
+                eventLogFunctions.writeToSystemEventLog(String.Format("The server responded with an HTTP error while accessing the URL {0}{1}{0}. This may be because the web site is down or some other kind of issue. Please check back at at later time.", Chr(34), lastAccessedURL), EventLogEntryType.Warning)
+            ElseIf TypeOf ex Is sslErrorException Then
+                eventLogFunctions.writeToSystemEventLog("An HTTP SSL error occurred.", EventLogEntryType.Error)
             End If
         End Sub
 
