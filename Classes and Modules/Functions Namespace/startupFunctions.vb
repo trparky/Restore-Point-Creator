f7259995564eed85becdb5474e667c60a8a08e2a
Tom <Tom@TOMS-DESKTOP>
2016-10-31 12:59:21 -0400

    Missed a few lines that needed to be changed.

--- Classes and Modules/Functions Namespace/startupFunctions.vb
+++ Classes and Modules/Functions Namespace/startupFunctions.vb
@@ -12,7 +12,7 @@ Namespace Functions.startupFunctions
             Dim windowsXPVersionSHA1URL As String = "http://www.toms-world.org/download/Restore Point Creator (Windows XP).exe.sha1"
 
             If MsgBox("This version of System Restore Point Creator is NOT compatible with Windows XP and will NOT run on Windows XP." & vbCrLf & vbCrLf & "Would you like to go back to the last version that supported Windows XP?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "System Restore Point Creator Windows XP Support") = MsgBoxResult.Yes Then
-                wait.createPleaseWaitWindow("Downloading File... Please Wait.", True)
+                wait.createPleaseWaitWindow("Downloading File... Please Wait.", enums.howToCenterWindow.screen, True)
 
                 If http.downloadFile("http://www.toms-world.org/download/updater.exe", "updater.exe") = False Then
                     wait.closePleaseWaitWindow()
