bed31b639c0cf2428c6dac3bc187847c0e2c6918
Tom <Tom@TOMS-DESKTOP>
2016-11-19 15:39:09 -0500

    Added the ability to auto-elevate the task when executing the program with the "-createrestorepoint" switch along with the "-name" parameter. This was broken in the past, thanks goes out to Surfer7 for reporting this issue to me.

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -878,6 +878,18 @@ Namespace My
                 Me("httpTimeout") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("")>  _
+        Public Property savedRestorePointFromCommandLine() As String
+            Get
+                Return CType(Me("savedRestorePointFromCommandLine"),String)
+            End Get
+            Set
+                Me("savedRestorePointFromCommandLine") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
