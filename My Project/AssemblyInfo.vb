c10ccc0e34a540fbf9f22ffe9767fa54bb485e1a
Tom <Tom@TOMS-DESKTOP>
2016-11-23 21:16:53 -0500

    Bumped version to 5.3 Build 9 Public Beta 8.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("5.3.8.0")>
+<Assembly: AssemblyFileVersion("5.3.9.0")>
