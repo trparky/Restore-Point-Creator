1088ff629ad69bc5bb34467d7edc509a2270a05e
Tom <Tom@TOMS-DESKTOP>
2016-11-26 18:54:58 -0500

    Finally added a long requested feature to the program, the ability to confirm the deletion of selected restore points in a batch.

---
+++ Windows/Confirm Restore Point Deletions Form.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Confirm_Restore_Point_Deletions_Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Confirm_Restore_Point_Deletions_Form))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.restorePointGroup = New System.Windows.Forms.Panel()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(614, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'restorePointGroup
+        '
+        Me.restorePointGroup.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.restorePointGroup.AutoScroll = True
+        Me.restorePointGroup.Location = New System.Drawing.Point(15, 38)
+        Me.restorePointGroup.Name = "restorePointGroup"
+        Me.restorePointGroup.Size = New System.Drawing.Size(696, 161)
+        Me.restorePointGroup.TabIndex = 1
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.Location = New System.Drawing.Point(338, 204)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(247, 23)
+        Me.btnCancel.TabIndex = 2
+        Me.btnCancel.Text = "&Cancel Deletion of All Selected Restore Points"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnConfirm.Location = New System.Drawing.Point(591, 204)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(120, 23)
+        Me.btnConfirm.TabIndex = 3
+        Me.btnConfirm.Text = "Confirm &Deletions"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'Confirm_Restore_Point_Deletions_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(723, 234)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.restorePointGroup)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = Global.Restore_Point_Creator.My.Resources.Resources.RestorePoint_noBackground_2
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(739, 273)
+        Me.Name = "Confirm_Restore_Point_Deletions_Form"
+        Me.Text = "Confirm Restore Point Deletions"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents restorePointGroup As Panel
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnConfirm As Button
+End Class
---
+++ Windows/Confirm Restore Point Deletions Form.vb
@@ -0,0 +1,89 @@
+﻿Public Class Confirm_Restore_Point_Deletions_Form
+    Public Enum userResponseENum
+        yes
+        cancel
+    End Enum
+
+    Public restorePointIDsToBeDeleted As New Dictionary(Of String, restorePointInfo)
+    Public userResponse As userResponseENum = userResponseENum.cancel
+
+    Sub scanCheckBoxesForUncheckedBoxes()
+        Dim checkBoxObject As CheckBox
+        Dim numberOfCheckboxes As Short = restorePointGroup.Controls.Count
+        Dim numberOfUncheckedCheckboxes As Short = 0
+
+        For index = (restorePointGroup.Controls.Count - 1) To 0 Step -1
+            If restorePointGroup.Controls(index).GetType = GetType(CheckBox) Then
+                checkBoxObject = DirectCast(restorePointGroup.Controls(index), CheckBox)
+
+                If Not checkBoxObject.Checked Then numberOfUncheckedCheckboxes += 1
+            End If
+        Next index
+
+        If numberOfUncheckedCheckboxes = numberOfCheckboxes Then
+            btnConfirm.Enabled = False
+            MsgBox("You have unchecked all of the checkboxes, you have none of your previously selected restore points queued for deletion.", MsgBoxStyle.Information, Me.Text)
+        Else
+            btnConfirm.Enabled = True
+        End If
+    End Sub
+
+    Sub createCheckbox(restorePointInfo As KeyValuePair(Of String, restorePointInfo), xPosition As Integer, yPosition As Integer)
+        Dim chkBox As New CheckBox
+        chkBox.Name = "confirm_delete_" & restorePointInfo.Key
+        chkBox.Location = New Point(xPosition, yPosition)
+        chkBox.Text = restorePointInfo.Value.strName & " (ID: " & restorePointInfo.Key & ")" & vbCrLf & "Created On: " & restorePointInfo.Value.strCreatedDate
+        chkBox.AutoSize = True
+        chkBox.Checked = True
+
+        AddHandler chkBox.Click, Sub() scanCheckBoxesForUncheckedBoxes()
+
+        restorePointGroup.Invoke(Sub() restorePointGroup.Controls.Add(chkBox))
+
+        chkBox = Nothing
+    End Sub
+
+    Private Sub Confirm_Restore_Point_Deletions_Form_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Dim yPosition As Integer = 10
+        Dim xPosition As Integer = 12
+
+        For Each item As KeyValuePair(Of String, restorePointInfo) In restorePointIDsToBeDeleted
+            createCheckbox(item, xPosition, yPosition)
+            yPosition += 40
+        Next
+    End Sub
+
+    Private Sub Confirm_Restore_Point_Deletions_Form_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.Width <> 739 Then Me.Width = 739
+    End Sub
+
+    Private Sub Confirm_Restore_Point_Deletions_Form_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        My.Settings.batchConfirmWindowSize = Me.Size
+    End Sub
+
+    Private Sub Confirm_Restore_Point_Deletions_Form_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.Size = My.Settings.batchConfirmWindowSize
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        userResponse = userResponseENum.yes
+        Dim checkBoxObject As CheckBox, strCheckBoxID As String
+
+        For index = (restorePointGroup.Controls.Count - 1) To 0 Step -1
+            If restorePointGroup.Controls(index).GetType = GetType(CheckBox) Then
+                checkBoxObject = DirectCast(restorePointGroup.Controls(index), CheckBox)
+
+                If Not checkBoxObject.Checked Then
+                    strCheckBoxID = checkBoxObject.Name.Replace("confirm_delete_", "").Trim
+                    If restorePointIDsToBeDeleted.Keys.Contains(strCheckBoxID) Then restorePointIDsToBeDeleted.Remove(strCheckBoxID)
+                End If
+            End If
+        Next index
+
+        Me.Close()
+    End Sub
+End Class
