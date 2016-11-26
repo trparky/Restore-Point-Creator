ccaddd88b5d31f4f1234be20b884b35c24183b1e
Tom <Tom@TOMS-DESKTOP>
2016-11-26 18:59:48 -0500

    Bumped version to 5.3 Build 14 Public Beta 13.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -65,7 +65,7 @@ Namespace globalVariables
             Public Const boolDebugBuild As Boolean = True
 
             Public Const boolBeta As Boolean = True
-            Public Const shortBetaVersion As Short = 12
+            Public Const shortBetaVersion As Short = 13
 
             Public Const boolReleaseCandidate As Boolean = False
             Public Const shortReleaseCandidateVersion As Short = 0
