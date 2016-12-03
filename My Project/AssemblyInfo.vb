8616fbcf9457668752a98772e2fc666997d05f35
Tom <Tom@TOMS-DESKTOP>
2016-12-02 23:02:17 -0500

    Bumped version to 5.4 Build 5 Public Beta 4.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("5.4.4.0")>
+<Assembly: AssemblyFileVersion("5.4.5.0")>
