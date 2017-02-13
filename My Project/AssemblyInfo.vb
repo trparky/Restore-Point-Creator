3fc71d35cd2c38d78d551885e2128199503694f2
Tom <Tom@TOMS-DESKTOP>
2017-02-13 08:54:30 -0500

    Bumped version to 5.9 Build 1.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("5.8.18.0")>
+<Assembly: AssemblyFileVersion("5.9.1.0")>
