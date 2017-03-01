f75536afaf3ad9cf387362f053b1ca7eb412f77e
Tom <Tom@TOMS-DESKTOP>
2017-03-01 15:37:01 -0500

    Bumped version to 6.0 Build 12 Public Beta 3.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.0.11.0")>
+<Assembly: AssemblyFileVersion("6.0.12.0")>
