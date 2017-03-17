0cd663c44841eeae7666ce08335ce723a08ec879
Tom <Tom@TOMS-DESKTOP>
2017-03-16 21:22:46 -0400

    Added some Code Analysis suppression code.

--- GlobalSuppressions.vb
+++ GlobalSuppressions.vb
@@ -4,4 +4,24 @@
 ' Project-level suppressions either have no target or are given 
 ' a specific target and scoped to a namespace, type, member, etc.
 
-<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")>
+<Assembly: CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")>
+<Assembly: CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.Functions.BCD.bcdEditorStatusException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.Functions.BCD.bcdEditorConnectionException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.Functions.myExceptions.myExceptions+integerTryParseException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.noMimeTypeFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.dataMissingException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.dnsLookupError")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.noHTTPServerResponseHeadersFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.sslErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.proxyConfigurationErrorException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.dataAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.credentialsAlreadySet")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.httpProtocolException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.noSSLCertificateFoundException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.myListViewItemTypes.volumeShadowCopyListItem")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.myListViewItemTypes.eventLogListEntry")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.localFileAlreadyExistsException")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.myListViewItemTypes.restorePointEntryItem")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId:="boldFont", Scope:="member", Target:="Restore_Point_Creator.Disk_Space_Usage.#Dispose(System.Boolean)")>
+<Assembly: CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2237:MarkISerializableTypesWithSerializable", Scope:="type", Target:="Restore_Point_Creator.Functions.BCD.bcdEditorSetException")>
