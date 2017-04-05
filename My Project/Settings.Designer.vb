907e4b7463e57d755f498fa2d93e1b7918991e2e
Tom <Tom@TOMS-DESKTOP>
2017-04-05 09:34:27 -0400

    Added a new setting to save the location of the update notification window.

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -950,6 +950,18 @@ Namespace My
                 Me("ignoreThisVersion") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("0, 0")>  _
+        Public Property updateNotificationWindowLocation() As Global.System.Drawing.Point
+            Get
+                Return CType(Me("updateNotificationWindowLocation"),Global.System.Drawing.Point)
+            End Get
+            Set
+                Me("updateNotificationWindowLocation") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
