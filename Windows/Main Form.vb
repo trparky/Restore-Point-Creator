4dcd9b32ba63e0cebccacacd2dd2f9d90f061e27
Tom <Tom@TOMS-DESKTOP>
2017-04-28 09:30:18 -0400

    Added copyright line for the task scheduler library to the About box.

--- Windows/Main Form.vb
+++ Windows/Main Form.vb
@@ -2866,6 +2866,8 @@ Public Class Form1
         stringBuilder.AppendLine("Written By Tom Parkison")
         stringBuilder.AppendLine("Copyright Thomas Parkison 2012-2018.")
         stringBuilder.AppendLine()
+        stringBuilder.AppendLine("This program uses the Microsoft.Win32.TaskScheduler library version 2.5.23 to interface with the Windows Task Scheduler, copyright David Hall.")
+        stringBuilder.AppendLine()
 
         If globalVariables.version.boolBeta Then
             stringBuilder.AppendFormat("Version {0} Public Beta {1}", globalVariables.version.strFullVersionString, globalVariables.version.shortBetaVersion)
