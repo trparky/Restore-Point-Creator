a5007154abbd4058923365c1ec8f08bb3c40b24c
Tom <Tom@TOMS-DESKTOP>
2017-06-07 19:33:43 -0400

    Merged in version 1.285 of the HTTPHelper.

--- Classes and Modules/HTTPHelper.vb
+++ Classes and Modules/HTTPHelper.vb
@@ -235,7 +235,7 @@ End Class
 
 ''' <summary>Allows you to easily POST and upload files to a remote HTTP server without you, the programmer, knowing anything about how it all works. This class does it all for you. It handles adding a User Agent String, additional HTTP Request Headers, string data to your HTTP POST data, and files to be uploaded in the HTTP POST data.</summary>
 Public Class httpHelper
-    Private Const classVersion As String = "1.280"
+    Private Const classVersion As String = "1.285"
 
     Private strUserAgentString As String = Nothing
     Private boolUseProxy As Boolean = False
@@ -899,7 +899,6 @@ beginAgain:
             Dim responseStream As Stream = webResponse.GetResponseStream() ' Gets the response stream.
 
             Dim lngBytesReadFromInternet As ULong = CType(responseStream.Read(dataBuffer, 0, dataBuffer.Length), ULong) ' Reads some data from the HTTP stream into our data buffer.
-            Dim oldCurrentFileSize As Long = 0
 
             ' We keep looping until all of the data has been downloaded.
             While lngBytesReadFromInternet <> 0
@@ -1029,7 +1028,6 @@ beginAgain:
             fileWriteStream = New FileStream(localFileName, FileMode.Create) ' Creates a file write stream.
 
             Dim lngBytesReadFromInternet As ULong = CType(responseStream.Read(dataBuffer, 0, dataBuffer.Length), ULong) ' Reads some data from the HTTP stream into our data buffer.
-            Dim oldCurrentFileSize As Long = 0
 
             ' We keep looping until all of the data has been downloaded.
             While lngBytesReadFromInternet <> 0
