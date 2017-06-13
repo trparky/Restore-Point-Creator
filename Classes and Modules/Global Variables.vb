208479fdfa103b8b9ef6194a7dfd9244365d2f88
Tom <Tom@TOMS-DESKTOP>
2017-06-13 10:56:44 -0400

    Bumped version to 6.2 Build 18 Release Candidate 1.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -77,11 +77,11 @@ Namespace globalVariables
 #If DEBUG Then
             Public Const boolDebugBuild As Boolean = True
 
-            Public Const boolBeta As Boolean = True
-            Public Const shortBetaVersion As Short = 8
+            Public Const boolBeta As Boolean = False
+            Public Const shortBetaVersion As Short = 0
 
-            Public Const boolReleaseCandidate As Boolean = False
-            Public Const shortReleaseCandidateVersion As Short = 0
+            Public Const boolReleaseCandidate As Boolean = True
+            Public Const shortReleaseCandidateVersion As Short = 1
 #Else
             Public Const boolDebugBuild As Boolean = False
 
