d0152947d91ff819b07fa4fd17c75578376c54da
Tom <Tom@TOMS-DESKTOP>
2017-08-23 18:04:21 -0400

    Bumped version to 6.3 Build 36 Public Beta 27.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.3.35.0")>
+<Assembly: AssemblyFileVersion("6.3.36.0")>
