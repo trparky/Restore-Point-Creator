f387e44ffbd85bf319885cc008c9782447fc5c27
Tom <Tom@TOMS-DESKTOP>
2017-09-23 10:53:24 -0400

    Removed a lot of low level API calls to determine if a user is part of the Administrator group and replaced it with much simpler native .NET API calls.

--- Classes and Modules/Functions Namespace/privilegeChecks.vb
+++ Classes and Modules/Functions Namespace/privilegeChecks.vb
@@ -71,100 +71,12 @@ Namespace Functions.privilegeChecks
         End Function
 
         Public Function IsUserInAdminGroup() As Boolean
-            Dim fInAdminGroup As Boolean = False
-            Dim hToken As NativeMethod.SafeTokenHandle = Nothing
-            Dim hTokenToCheck As NativeMethod.SafeTokenHandle = Nothing
-            Dim pElevationType As IntPtr = IntPtr.Zero
-            Dim pLinkedToken As IntPtr = IntPtr.Zero
-            Dim cbSize As Integer = 0
-
             Try
-                ' Open the access token of the current process for query and duplicate.
-                If (Not NativeMethod.NativeMethod.OpenProcessToken(Process.GetCurrentProcess.Handle, NativeMethod.NativeMethod.TOKEN_QUERY Or NativeMethod.NativeMethod.TOKEN_DUPLICATE, hToken)) Then
-                    Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                End If
-
-                ' Determine whether system is running Windows Vista or later operating
-                ' systems (major version >= 6) because they support linked tokens, but
-                ' previous versions (major version < 6) do not.
-                If (Environment.OSVersion.Version.Major >= 6) Then
-                    ' Running Windows Vista or later (major version >= 6).
-                    ' Determine token type: limited, elevated, or default.
-
-                    ' Allocate a buffer for the elevation type information.
-                    cbSize = 4  ' Size of TOKEN_ELEVATION_TYPE
-                    pElevationType = Runtime.InteropServices.Marshal.AllocHGlobal(cbSize)
-                    If (pElevationType = IntPtr.Zero) Then
-                        Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                    End If
-
-                    ' Retrieve token elevation type information.
-                    If (Not NativeMethod.NativeMethod.GetTokenInformation(hToken, NativeMethod.TOKEN_INFORMATION_CLASS.TokenElevationType, pElevationType, cbSize, cbSize)) Then
-                        Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                    End If
-
-                    ' Marshal the TOKEN_ELEVATION_TYPE enum from native to .NET.
-                    Dim elevType As NativeMethod.TOKEN_ELEVATION_TYPE = Runtime.InteropServices.Marshal.ReadInt32(pElevationType)
-
-                    ' If limited, get the linked elevated token for further check.
-                    If (elevType = Functions.NativeMethod.TOKEN_ELEVATION_TYPE.TokenElevationTypeLimited) Then
-                        ' Allocate a buffer for the linked token.
-                        cbSize = IntPtr.Size
-                        pLinkedToken = Runtime.InteropServices.Marshal.AllocHGlobal(cbSize)
-                        If (pLinkedToken = IntPtr.Zero) Then
-                            Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                        End If
-
-                        ' Get the linked token.
-                        If (Not NativeMethod.NativeMethod.GetTokenInformation(hToken, NativeMethod.TOKEN_INFORMATION_CLASS.TokenLinkedToken, pLinkedToken, cbSize, cbSize)) Then
-                            Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                        End If
-
-                        ' Marshal the linked token value from native to .NET.
-                        Dim hLinkedToken As IntPtr = Runtime.InteropServices.Marshal.ReadIntPtr(pLinkedToken)
-                        hTokenToCheck = New NativeMethod.SafeTokenHandle(hLinkedToken)
-                    End If
-                End If
-
-                ' CheckTokenMembership requires an impersonation token. If we just got a
-                ' linked token, it already is an impersonation token.  If we did not get
-                ' a linked token, duplicate the original into an impersonation token for
-                ' CheckTokenMembership.
-                If (hTokenToCheck Is Nothing) Then
-                    If (Not NativeMethod.NativeMethod.DuplicateToken(hToken, NativeMethod.SECURITY_IMPERSONATION_LEVEL.SecurityIdentification, hTokenToCheck)) Then
-                        Throw New ComponentModel.Win32Exception(Runtime.InteropServices.Marshal.GetLastWin32Error)
-                    End If
-                End If
-
-                ' Check if the token to be checked contains admin SID.
-                Dim id As New WindowsIdentity(hTokenToCheck.DangerousGetHandle)
-                Dim principal As New WindowsPrincipal(id)
-                fInAdminGroup = principal.IsInRole(WindowsBuiltInRole.Administrator)
+                Return (WindowsIdentity.GetCurrent().Groups.Where(Function(identityReference As IdentityReference) identityReference.Value.Trim.Equals("S-1-5-32-544", StringComparison.OrdinalIgnoreCase)).Count > 0)
             Catch ex As Exception
-                ' Something went wrong here so we are going to assume that the user isn't part of the Administrator user group.
-                fInAdminGroup = False
-                eventLogFunctions.writeCrashToEventLog(ex) ' Write exception data to the Windows Event Log.
-            Finally
-                ' Centralized cleanup for all allocated resources.
-                If (Not hToken Is Nothing) Then
-                    hToken.Close()
-                    hToken = Nothing
-                End If
-                If (Not hTokenToCheck Is Nothing) Then
-                    hTokenToCheck.Close()
-                    hTokenToCheck = Nothing
-                End If
-                If (pElevationType <> IntPtr.Zero) Then
-                    Runtime.InteropServices.Marshal.FreeHGlobal(pElevationType)
-                    pElevationType = IntPtr.Zero
-                End If
-                If (pLinkedToken <> IntPtr.Zero) Then
-                    Runtime.InteropServices.Marshal.FreeHGlobal(pLinkedToken)
-                    pLinkedToken = IntPtr.Zero
-                End If
+                eventLogFunctions.writeCrashToEventLog(ex)
+                Return False
             End Try
-
-            Return fInAdminGroup
         End Function
     End Module
 End Namespace
