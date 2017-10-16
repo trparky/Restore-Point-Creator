dc3155619e9e19627e5f6825bba17855b84cc15a
Tom <Tom@TOMS-DESKTOP>
2017-10-16 19:28:17 -0400

    Bumped version to 6.5 Build 17 Public Beta 8.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.5.16.0")>
+<Assembly: AssemblyFileVersion("6.5.17.0")>
