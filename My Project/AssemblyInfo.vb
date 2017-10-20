16c736b856db5f3c316da1920eae3db2895baca2
Tom <Tom@TOMS-DESKTOP>
2017-10-19 21:17:09 -0400

    Bumped version to 6.5 Build 20 Public Beta 11.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.5.19.0")>
+<Assembly: AssemblyFileVersion("6.5.20.0")>
