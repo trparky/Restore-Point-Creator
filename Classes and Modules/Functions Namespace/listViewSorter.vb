7c0f42b17a4400cf02a4c12cc4ae41740893af85
Tom <Tom@TOMS-DESKTOP>
2017-11-06 22:02:44 -0500

    Included code in the listview sorter code to specifically handle the application event log ID column value.

--- Classes and Modules/Functions Namespace/listViewSorter.vb
+++ Classes and Modules/Functions Namespace/listViewSorter.vb
@@ -1,13 +1,10 @@
-﻿Imports System.Text.RegularExpressions
-
-Namespace Functions.listViewSorter
+﻿Namespace Functions.listViewSorter
     ' Implements a comparer for ListView columns.
     Class ListViewComparer
         Implements IComparer
 
         Private intColumnNumber As Integer
         Private soSortOrder As SortOrder
-        Private regExNumberParser As New Regex("\A[,0-9]+\Z", RegexOptions.Compiled) '
 
         Public Sub New(ByVal intInputColumnNumber As Integer, ByVal soInputSortOrder As SortOrder)
             intColumnNumber = intInputColumnNumber
@@ -40,13 +37,6 @@ Namespace Functions.listViewSorter
                 strSecondString = lvSecondListView.SubItems(intColumnNumber).Text
             End If
 
-            If regExNumberParser.IsMatch(strFirstString) Then
-                strFirstString = strFirstString.Replace(",", "")
-            End If
-            If regExNumberParser.IsMatch(strSecondString) Then
-                strSecondString = strSecondString.Replace(",", "")
-            End If
-
             ' Compare them.
             If soSortOrder = SortOrder.Ascending Then
                 If lvFirstListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) Then
@@ -59,6 +49,11 @@ Namespace Functions.listViewSorter
                     date2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).logDate
 
                     Return date1.CompareTo(date2)
+                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 2 Then
+                    long1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+                    long2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+
+                    Return long1.CompareTo(long2)
                 ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And intColumnNumber = 0 Then
                     int1 = DirectCast(lvFirstListView, myListViewItemTypes.restorePointEntryItem).intID
                     int2 = DirectCast(lvSecondListView, myListViewItemTypes.restorePointEntryItem).intID
@@ -86,6 +81,11 @@ Namespace Functions.listViewSorter
                     date2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).logDate
 
                     Return date2.CompareTo(date1)
+                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 2 Then
+                    long1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+                    long2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+
+                    Return long2.CompareTo(long1)
                 ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And intColumnNumber = 0 Then
                     int1 = DirectCast(lvFirstListView, myListViewItemTypes.restorePointEntryItem).intID
                     int2 = DirectCast(lvSecondListView, myListViewItemTypes.restorePointEntryItem).intID
