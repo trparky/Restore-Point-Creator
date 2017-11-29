d48be73f30b704ab378d4c874e01ffa385b12559
Tom <Tom@TOMS-DESKTOP>
2017-11-29 17:35:28 -0500

    Bumped version to 6.7 Build 14 Public Beta 5.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -79,7 +79,7 @@ Namespace globalVariables
             Public Const boolDebugBuild As Boolean = True
 
             Public Const boolBeta As Boolean = True
-            Public Const shortBetaVersion As Short = 4
+            Public Const shortBetaVersion As Short = 5
 
             Public Const boolReleaseCandidate As Boolean = False
             Public Const shortReleaseCandidateVersion As Short = 0
--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.7.13.0")>
+<Assembly: AssemblyFileVersion("6.7.14.0")>
