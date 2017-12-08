a0a5c817735d96c0d97b80cc1000d3f50952de8c
Tom <Tom@TOMS-DESKTOP>
2017-12-07 21:44:14 -0500

    Optimized the bytesToHumanSize() function to use less code.

--- Classes and Modules/Functions Namespace/support.vb
+++ Classes and Modules/Functions Namespace/support.vb
@@ -577,39 +577,20 @@ Namespace Functions.support
         ''' <returns>A String value such as 100 MBs or 100 GBs.</returns>
         Public Function bytesToHumanSize(ByVal size As ULong, Optional roundToNearestWholeNumber As Boolean = False) As String
             Dim result As String
+            Dim shortRoundNumber As Short = If(roundToNearestWholeNumber, 0, 2)
 
             If size <= (2 ^ 10) Then
                 result = size & " Bytes"
             ElseIf size > (2 ^ 10) And size <= (2 ^ 20) Then
-                If roundToNearestWholeNumber = True Then
-                    result = Math.Round(size / (2 ^ 10), 0) & " KBs"
-                Else
-                    result = Math.Round(size / (2 ^ 10), 2) & " KBs"
-                End If
+                result = Math.Round(size / (2 ^ 10), shortRoundNumber) & " KBs"
             ElseIf size > (2 ^ 20) And size <= (2 ^ 30) Then
-                If roundToNearestWholeNumber = True Then
-                    result = Math.Round(size / (2 ^ 20), 0) & " MBs"
-                Else
-                    result = Math.Round(size / (2 ^ 20), 2) & " MBs"
-                End If
+                result = Math.Round(size / (2 ^ 20), shortRoundNumber) & " MBs"
             ElseIf size > (2 ^ 30) And size <= (2 ^ 40) Then
-                If roundToNearestWholeNumber = True Then
-                    result = Math.Round(size / (2 ^ 30), 0) & " GBs"
-                Else
-                    result = Math.Round(size / (2 ^ 30), 2) & " GBs"
-                End If
+                result = Math.Round(size / (2 ^ 30), shortRoundNumber) & " GBs"
             ElseIf size > (2 ^ 40) And size <= (2 ^ 50) Then
-                If roundToNearestWholeNumber = True Then
-                    result = Math.Round(size / (2 ^ 40), 0) & " TBs"
-                Else
-                    result = Math.Round(size / (2 ^ 40), 2) & " TBs"
-                End If
+                result = Math.Round(size / (2 ^ 40), shortRoundNumber) & " TBs"
             ElseIf size > (2 ^ 50) And size <= (2 ^ 60) Then
-                If roundToNearestWholeNumber = True Then
-                    result = Math.Round(size / (2 ^ 50), 0) & " PBs"
-                Else
-                    result = Math.Round(size / (2 ^ 50), 2) & " PBs"
-                End If
+                result = Math.Round(size / (2 ^ 50), shortRoundNumber) & " PBs"
             Else
                 result = "(None)"
             End If
