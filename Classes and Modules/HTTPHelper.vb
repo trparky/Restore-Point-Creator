9a4d280ae6749d548bf5b1cefb02a64a4d92cdf1
Tom <Tom@TOMS-DESKTOP>
2017-12-08 20:28:17 -0500

    Merged in version 1.302 of the HTTPHelper to fix an issue.

--- Classes and Modules/HTTPHelper.vb
+++ Classes and Modules/HTTPHelper.vb
@@ -1,4 +1,5 @@
-﻿Imports System.IO
+﻿Option Strict Off
+Imports System.IO
 Imports System.Security.Cryptography
 
 Public Class FormFile
@@ -235,7 +236,7 @@ End Class
 
 ''' <summary>Allows you to easily POST and upload files to a remote HTTP server without you, the programmer, knowing anything about how it all works. This class does it all for you. It handles adding a User Agent String, additional HTTP Request Headers, string data to your HTTP POST data, and files to be uploaded in the HTTP POST data.</summary>
 Public Class httpHelper
-    Private Const classVersion As String = "1.301"
+    Private Const classVersion As String = "1.302"
 
     Private strUserAgentString As String = Nothing
     Private boolUseProxy As Boolean = False
