eb47108400b8666a749add4e3cd99d0874333b61
Tom <Tom@TOMS-DESKTOP>
2017-12-09 12:09:49 -0500

    Bumped version to 6.7 Build 24 Release Candidate 6.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -82,7 +82,7 @@ Namespace globalVariables
             Public Const shortBetaVersion As Short = 0
 
             Public Const boolReleaseCandidate As Boolean = True
-            Public Const shortReleaseCandidateVersion As Short = 5
+            Public Const shortReleaseCandidateVersion As Short = 6
 #Else
             Public Const boolDebugBuild As Boolean = False
 
