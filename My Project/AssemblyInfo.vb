fb2f783e286a1f40c82a4c08af9b4bda3f2a0fb9
Tom <Tom@TOMS-DESKTOP>
2017-12-17 13:29:35 -0500

    Bumped version to 6.7 Build 33 Release Candidate 15.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.7.32.0")>
+<Assembly: AssemblyFileVersion("6.7.33.0")>
