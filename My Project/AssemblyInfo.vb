82a0c28fa2ff61557a8ed4ba13f113a01d869f67
Tom <Tom@TOMS-DESKTOP>
2018-02-04 15:06:40 -0500

    Bumped version to 6.9 Build 18 Public Beta 9.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("6.9.17.0")>
+<Assembly: AssemblyFileVersion("6.9.18.0")>
