734483b9bba63521c568c834e312b547d384c573
Tom <Tom@TOMS-DESKTOP>
2018-02-13 16:27:17 -0500

    Bumped version to 6.9 Build 26 Public Beta 17.

--- Classes and Modules/Global Variables.vb
+++ Classes and Modules/Global Variables.vb
@@ -79,7 +79,7 @@ Namespace globalVariables
             Public Const boolDebugBuild As Boolean = True
 
             Public Const boolBeta As Boolean = True
-            Public Const shortBetaVersion As Short = 16
+            Public Const shortBetaVersion As Short = 17
 
             Public Const boolReleaseCandidate As Boolean = False
             Public Const shortReleaseCandidateVersion As Short = 0
