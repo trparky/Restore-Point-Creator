f5d9d144120d2fe0a32b267ce917e60a2f4bcfed
Tom <Tom@TOMS-DESKTOP>
2018-02-18 13:17:05 -0500

    Bumped version to 7.0 Build 13 Public Beta 4.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("7.0.12.0")>
+<Assembly: AssemblyFileVersion("7.0.13.0")>
