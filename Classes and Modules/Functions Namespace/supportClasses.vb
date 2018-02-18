b8e018f7cb4ff7bd6767ddc58c58928d79d616e2
Tom <Tom@TOMS-DESKTOP>
2018-02-18 10:26:36 -0500

    Added human readable versions of many of the data points for the ShadowStorageData class.

--- Classes and Modules/Functions Namespace/supportClasses.vb
+++ Classes and Modules/Functions Namespace/supportClasses.vb
@@ -5,6 +5,9 @@
         Public MaxSpace As ULong = 0
         Public UsedSpace As ULong = 0
         Public Volume As String = Nothing
+        Public UsedSpaceHuman As String = Nothing
+        Public MaxSpaceHuman As String = Nothing
+        Public AllocatedSpaceHuman As String = Nothing
     End Class
 
     Public Class processorInfoClass
