3dd4e822d1770f06805b0676b8b68d7175956809
Tom <Tom@TOMS-DESKTOP>
2018-03-10 21:57:05 -0500

    Cleaned up the listview sorting code to use less code.

--- Classes and Modules/Functions Namespace/listViewSorter.vb
+++ Classes and Modules/Functions Namespace/listViewSorter.vb
@@ -29,69 +29,35 @@
             strSecondString = If(lvSecondListView.SubItems.Count <= intColumnNumber, "", lvSecondListView.SubItems(intColumnNumber).Text)
 
             ' Compare them.
-            If soSortOrder = SortOrder.Ascending Then
-                If lvFirstListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) Then
-                    Dim vol1 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvFirstListView, myListViewItemTypes.volumeShadowCopyListItem)
-                    Dim vol2 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvSecondListView, myListViewItemTypes.volumeShadowCopyListItem)
+            If lvFirstListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) Then
+                Dim vol1 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvFirstListView, myListViewItemTypes.volumeShadowCopyListItem)
+                Dim vol2 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvSecondListView, myListViewItemTypes.volumeShadowCopyListItem)
 
-                    Return vol1.dateCreated.CompareTo(vol2.dateCreated)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 1 Then
-                    date1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).logDate
-                    date2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).logDate
+                Return If(soSortOrder = SortOrder.Ascending, vol1.dateCreated.CompareTo(vol2.dateCreated), vol2.dateCreated.CompareTo(vol1.dateCreated))
+            ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 1 Then
+                date1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).logDate
+                date2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).logDate
 
-                    Return date1.CompareTo(date2)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 2 Then
-                    long1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
-                    long2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+                Return If(soSortOrder = SortOrder.Ascending, date1.CompareTo(date2), date2.CompareTo(date1))
+            ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 2 Then
+                long1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
+                long2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
 
-                    Return long1.CompareTo(long2)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And intColumnNumber = 0 Then
-                    int1 = DirectCast(lvFirstListView, myListViewItemTypes.restorePointEntryItem).intID
-                    int2 = DirectCast(lvSecondListView, myListViewItemTypes.restorePointEntryItem).intID
+                Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
+            ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And intColumnNumber = 0 Then
+                int1 = DirectCast(lvFirstListView, myListViewItemTypes.restorePointEntryItem).intID
+                int2 = DirectCast(lvSecondListView, myListViewItemTypes.restorePointEntryItem).intID
 
-                    Return int1.CompareTo(int2)
-                Else
-                    If Double.TryParse(strFirstString, dbl1) And Double.TryParse(strSecondString, dbl2) Then
-                        Return dbl1.CompareTo(dbl2)
-                    ElseIf Date.TryParse(strFirstString, date1) And Date.TryParse(strSecondString, date2) Then
-                        Return date1.CompareTo(date2)
-                    ElseIf Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
-                        Return long1.CompareTo(long2)
-                    Else
-                        Return String.Compare(strFirstString, strSecondString)
-                    End If
-                End If
+                Return If(soSortOrder = SortOrder.Ascending, int1.CompareTo(int2), int2.CompareTo(int1))
             Else
-                If lvFirstListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.volumeShadowCopyListItem)) Then
-                    Dim vol1 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvFirstListView, myListViewItemTypes.volumeShadowCopyListItem)
-                    Dim vol2 As myListViewItemTypes.volumeShadowCopyListItem = DirectCast(lvSecondListView, myListViewItemTypes.volumeShadowCopyListItem)
-
-                    Return vol2.dateCreated.CompareTo(vol1.dateCreated)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 1 Then
-                    date1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).logDate
-                    date2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).logDate
-
-                    Return date2.CompareTo(date1)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.eventLogListEntry)) And intColumnNumber = 2 Then
-                    long1 = DirectCast(lvFirstListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
-                    long2 = DirectCast(lvSecondListView, myListViewItemTypes.eventLogListEntry).longEventLogEntryID
-
-                    Return long2.CompareTo(long1)
-                ElseIf lvFirstListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And lvSecondListViewType.Equals(GetType(myListViewItemTypes.restorePointEntryItem)) And intColumnNumber = 0 Then
-                    int1 = DirectCast(lvFirstListView, myListViewItemTypes.restorePointEntryItem).intID
-                    int2 = DirectCast(lvSecondListView, myListViewItemTypes.restorePointEntryItem).intID
-
-                    Return int2.CompareTo(int1)
+                If Double.TryParse(strFirstString, dbl1) And Double.TryParse(strSecondString, dbl2) Then
+                    Return If(soSortOrder = SortOrder.Ascending, dbl1.CompareTo(dbl2), dbl2.CompareTo(dbl1))
+                ElseIf Date.TryParse(strFirstString, date1) And Date.TryParse(strSecondString, date2) Then
+                    Return If(soSortOrder = SortOrder.Ascending, date1.CompareTo(date2), date2.CompareTo(date1))
+                ElseIf Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
+                    Return If(soSortOrder = SortOrder.Ascending, long1.CompareTo(long2), long2.CompareTo(long1))
                 Else
-                    If Double.TryParse(strFirstString, dbl1) And Double.TryParse(strSecondString, dbl2) Then
-                        Return dbl2.CompareTo(dbl1)
-                    ElseIf Date.TryParse(strFirstString, date1) And Date.TryParse(strSecondString, date2) Then
-                        Return date2.CompareTo(date1)
-                    ElseIf Long.TryParse(strFirstString, long1) And Long.TryParse(strSecondString, long2) Then
-                        Return long2.CompareTo(long1)
-                    Else
-                        Return String.Compare(strSecondString, strFirstString)
-                    End If
+                    Return If(soSortOrder = SortOrder.Ascending, String.Compare(strFirstString, strSecondString), String.Compare(strSecondString, strFirstString))
                 End If
             End If
         End Function
