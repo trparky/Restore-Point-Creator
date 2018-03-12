f875d0a8afb7c3b1a3cc1a4b66ac0210054a4144
Tom <Tom@TOMS-DESKTOP>
2018-03-12 11:25:16 -0400

    Bumped version to 7.0 Build 28 Public Beta 19.

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("7.0.27.0")>
+<Assembly: AssemblyFileVersion("7.0.28.0")>
